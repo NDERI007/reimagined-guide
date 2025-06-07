54d635c41f51ed9cccb686dfea4aa2df10492f4d
NDERI007 <tnderi373@gmail.com>
2025-06-07 13:02:49 +0300

    yet to learn drag and drop

--- Haijima/src/jOBs/jobLayout.tsx
+++ Haijima/src/jOBs/jobLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 import { JobProvider, addNewJob } from '../jOBs/JobsTORE';
 import Navbar from '../components/nav';
 import JobModal from './jobModal';
-import { ModalWrapper } from '../components/ModalWrapper';
+import ModalWrapper from '../components/ModalWrapper';
 
 const JobLayout = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
--- Haijima/src/jOBs/jobModal.tsx
+++ Haijima/src/jOBs/jobModal.tsx
@@ -28,48 +28,46 @@ const JobModal: React.FC<{
   };
 
   return (
-    <div className="bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center bg-black/60">
-      <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
-        <h2 className="mb-4 text-xl font-semibold">Add New Job</h2>
-        <input
-          className="mb-2 w-full rounded border border-gray-300 p-2 focus-within:outline-emerald-400 hover:border-emerald-200"
-          placeholder="Job Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          className="mb-2 w-full rounded border border-gray-300 p-2 focus-within:outline-emerald-400 hover:border-emerald-200"
-          placeholder="Company"
-          value={company}
-          onChange={(e) => setCompany(e.target.value)}
-        />
-        <input
-          className="mb-2 w-full rounded border border-gray-300 p-2 focus-within:outline-emerald-400 hover:border-emerald-200"
-          placeholder="Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <select
-          className="mb-4 w-full rounded border border-gray-300 p-2 outline-none"
-          value={job_status}
-          onChange={(e) => setStatus(e.target.value as JobStatus)}
-        >
-          <option value="Saved">Saved</option>
-          <option value="Interview">Interview</option>
-          <option value="Rejected">Rejected</option>
-        </select>
+    <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
+      <h2 className="mb-4 text-xl font-semibold">Add New Job</h2>
+      <input
+        className="mb-2 w-full rounded border border-gray-300 p-2 focus-within:outline-emerald-400 hover:border-emerald-200"
+        placeholder="Job Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        className="mb-2 w-full rounded border border-gray-300 p-2 focus-within:outline-emerald-400 hover:border-emerald-200"
+        placeholder="Company"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+      />
+      <input
+        className="mb-2 w-full rounded border border-gray-300 p-2 focus-within:outline-emerald-400 hover:border-emerald-200"
+        placeholder="Location"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <select
+        className="mb-4 w-full rounded border border-gray-300 p-2 outline-none"
+        value={job_status}
+        onChange={(e) => setStatus(e.target.value as JobStatus)}
+      >
+        <option value="Saved">Saved</option>
+        <option value="Interview">Interview</option>
+        <option value="Rejected">Rejected</option>
+      </select>
 
-        <div className="flex justify-end space-x-2">
-          <button className="rounded bg-gray-300 px-4 py-2" onClick={onClose}>
-            Cancel
-          </button>
-          <button
-            className="rounded bg-emerald-600 px-4 py-2 text-white"
-            onClick={handleSubmit}
-          >
-            Add Job
-          </button>
-        </div>
+      <div className="flex justify-end space-x-2">
+        <button className="rounded bg-gray-300 px-4 py-2" onClick={onClose}>
+          Cancel
+        </button>
+        <button
+          className="rounded bg-emerald-600 px-4 py-2 text-white"
+          onClick={handleSubmit}
+        >
+          Add Job
+        </button>
       </div>
     </div>
   );
